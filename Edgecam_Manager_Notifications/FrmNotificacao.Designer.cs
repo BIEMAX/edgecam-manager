496903051bc4ee061bb719c52e8d74a1fd5fcfd5
Dionei Beilke dos Santos <47108057+BIEMAX@users.noreply.github.com>
2021-02-25 16:41:22 -0300

    Add files via upload

---
+++ Edgecam_Manager_Notifications/FrmNotificacao.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Edgecam_Manager_Notifications
+{
+    partial class FrmNotificacao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // FrmNotificacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 152);
+            this.Name = "FrmNotificacao";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Edgecam_Manager_Notifications/FrmNotificacao.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Tulpep.NotificationWindow;
+
+namespace Edgecam_Manager_Notifications
+{
+    public partial class FrmNotificacao : Form
+    {
+        //Help: https://www.c-sharpcorner.com/article/working-with-popup-notification-in-windows-forms/
+
+        #region Variáveis globais
+
+        protected e_SkaVersao mVersaoMgr;
+
+        #endregion
+
+        #region Propriedades
+
+        #endregion
+
+        #region Enumeradores
+
+        public enum e_SkaVersao
+        {
+            V2017R1,
+            V2017R2,
+            V2018R1,
+            V2018R2,
+            V2019R1
+        }
+
+        #endregion
+
+        #region Instância dos objetos da classe
+
+        public FrmNotificacao()
+        {
+            InitializeComponent();
+
+            EscondeForm();
+
+            timer.Interval = 10000;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        /// <summary>
+        ///     Instância do objeto que carrega as notificações para o usuário.
+        /// </summary>
+        /// <remarks>Essa classe instância um Form, e eu escondo ele nessa chamada.</remarks>
+        public FrmNotificacao(e_SkaVersao Versao)
+        {
+            InitializeComponent();
+
+            EscondeForm();
+
+            timer.Interval = 10000;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        #endregion
+
+        #region Métodos
+
+        /// <summary>
+        ///     Esconde o formulário do usuário.
+        /// </summary>
+        private void EscondeForm()
+        {
+            this.Visible = false;
+            this.Opacity = 0;
+            this.ShowInTaskbar = false;
+            this.Hide();
+        }
+
+        private void MostraNotificacao()
+        {
+            //Tive de invocar esse popup, 
+            this.Invoke((MethodInvoker)delegate 
+            {
+                PopupNotifier popup = new PopupNotifier();
+
+                popup.Image = Properties.Resources.Edgecam_Manager.ToBitmap();
+
+                popup.BorderColor = Color.LightGreen;
+                popup.HeaderColor = Color.LightGreen;
+
+                popup.AnimationDuration = 5;//Em segundos
+
+                popup.TitleFont = new System.Drawing.Font("Arial", 18.0f, FontStyle.Bold);
+                popup.TitleText = "Edgecam Manager - 2018 R2";
+
+                popup.ContentFont = new System.Drawing.Font("Arial", 12.0f, FontStyle.Regular);
+                popup.ContentText = "Você possuí novas tarefas";
+
+                popup.Popup();// show
+            });
+        }
+
+        #endregion
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            MostraNotificacao();
+        }
+
+        #region Eventos
+
+        #endregion
+
+    }
+}
